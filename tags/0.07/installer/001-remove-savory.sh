facdb22e7221239bcb5a7f92043904bb3f4e3712
jyavenard <jyavenard@ff0d5fae-206c-11de-b650-e715bd6d7cf1>
2009-11-03 07:08:55 +0000

    tag release 0.07

---
+++ tags/0.07/installer/001-remove-savory.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+_FUNCTIONS=/etc/rc.d/functions
+[ -f ${_FUNCTIONS} ] && . ${_FUNCTIONS}
+
+. /etc/sysconfig/mntus
+
+VERSION_FILE=/etc/prettyversion.txt
+NF_INI=/opt/amazon/ebook/config/netfront.ini
+SAVORYCTL=/etc/init.d/savoryctl
+SAVORYLNK=/etc/rc5.d/S99savoryctl
+
+#Remove previous version backup files
+SAVE_NF_INI=${NF_INI}-beforesavory
+SAVE_PRETTY=${VERSION_FILE}-beforesavory
+rm -f $SAVE_PRETTY
+rm -f $SAVE_NF_INI
+
+if [ -f $SAVORYCTL ]; then
+    rm -f $SAVORYCTL 
+fi
+rm -f /etc/rc5.d/S99savoryctl
+
+update_progressbar 20
+
+if [ -f $NF_INI ]; then
+    VAR=$(grep -e '^dlexts=' $NF_INI)
+    VAR2=$(echo $VAR | sed -r -e 's: ?epub lrf lit pdf fb2 odt::g')
+    sed -i -r -e "s:$VAR:$VAR2:g" $NF_INI
+
+    VAR=$(grep -e '^dlmimes=' $NF_INI)
+    VAR2=$(echo $VAR | sed -r -e 's: ?application/pdf::g')
+    sed -i -r -e "s:$VAR:$VAR2:g" $NF_INI
+fi
+
+update_progressbar 60
+
+if [ -f $VERSION_FILE ]; then
+    sed -i -r -e 's/ UNSUPPORTED SAVORY-0.[0-9]+//g' $VERSION_FILE
+    sed -i -r -e 's/( +\+ Savory [0-9]\.[0-9]\.[0-9]|$)//g' $VERSION_FILE
+fi
+
+update_progressbar 100
+
+return 0
---
+++ tags/0.07/installer/001-savory.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+_FUNCTIONS=/etc/rc.d/functions
+[ -f ${_FUNCTIONS} ] && . ${_FUNCTIONS}
+
+. /etc/sysconfig/mntus
+
+VERSION_FILE=/etc/prettyversion.txt
+NF_INI=/opt/amazon/ebook/config/netfront.ini
+SAVORYCTL=/etc/init.d/savoryctl
+SAVORYLNK=/etc/rc5.d/S99savoryctl
+
+#Remove previous version backup files
+SAVE_NF_INI=${NF_INI}-beforesavory
+SAVE_PRETTY=${VERSION_FILE}-beforesavory
+rm -f $SAVE_PRETTY
+rm -f $SAVE_NF_INI
+
+update_progressbar 20 
+
+if [ -f $SAVORYCTL ]; then
+    rm -f $SAVORYCTL
+fi
+cp savoryctl $SAVORYCTL
+chown root:root $SAVORYCTL
+chmod 755 $SAVORYCTL
+
+rm -f $SAVORYLNK
+ln -s $SAVORYCTL $SAVORYLNK
+
+update_progressbar 50
+
+if [ -f $NF_INI ]; then
+    VAR=$(grep -e '^dlexts=' $NF_INI)
+    VAR2=$(echo $VAR | sed -r 's: ?epub lrf lit pdf fb2 odt::g' | sed -r 's:$: epub lrf lit pdf fb2 odt:g')
+    sed -i -r "s:$VAR:$VAR2:g" $NF_INI
+
+    VAR=$(grep -e '^dlmimes=' $NF_INI)
+    VAR2=$(echo $VAR | sed -r 's: ?application/pdf::g' | sed -r 's:$: application/pdf:g')
+    sed -i -r "s:$VAR:$VAR2:g" $NF_INI
+fi
+
+update_progressbar 60
+
+if [ -f $VERSION_FILE ]; then
+#Remove older style version name, and update version number
+    sed -i -r 's/ UNSUPPORTED SAVORY-0.[0-9]+//g' $VERSION_FILE
+    sed -i -r 's/( +\+ Savory [0-9]\.[0-9]\.[0-9]|$)/ \+ Savory 0.0.7/' $VERSION_FILE
+fi
+
+update_progressbar 100
+
+return 0
